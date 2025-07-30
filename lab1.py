691730bca7e083b54e0661278600ba6a224198d6
shubham06kumar <shubham.ok.06@gmail.com>
2025-07-30 22:48:58 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,3 +1,26 @@
-#Write your code here in python 
-n=int(input("enter a nuber"))
-print(n)
+# Grading System
+while True:
+    marks = input("Enter marks (0-100) or type 'Exit' to quit: ")
+
+    if marks.upper() == "EXIT":
+        print("Exiting the program.")
+        break
+
+    if not marks.isdigit():
+        print("Invalid input. Please enter a number between 0 and 100.")
+        continue
+
+    mark = int(marks)
+
+    if marks < 0 or marks > 100:
+        print("Invalid input. Marks should be between 0 and 100.")
+    elif marks >= 90:
+        print("Grade A")
+    elif marks >= 75:
+        print("Grade B")
+    elif marks >= 60:
+        print("Grade C")
+    elif marks >= 40:
+        print("Grade D")
+    else:
+        print("Grade F")
