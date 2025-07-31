8dd3bc0d750c7611ebc28df014db9ea74ac39e70
shubham06kumar <shubham.ok.06@gmail.com>
2025-07-31 09:47:05 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,26 +1,28 @@
 # Grading System
-while True:
-    marks = input("Enter marks (0-100) or type 'Exit' to quit: ")
+import sys
 
-    if marks.upper() == "EXIT":
-        print("Exiting the program.")
-        break
+marks = input()
 
-    if not marks.isdigit():
-        print("Invalid input. Please enter a number between 0 and 100.")
-        continue
+if marks.upper() == "EXIT":
+    print("Exiting the program.")
+    sys.exit()
 
-    mark = int(marks)
 
-    if marks < 0 or marks > 100:
-        print("Invalid input. Marks should be between 0 and 100.")
-    elif marks >= 90:
-        print("Grade A")
-    elif marks >= 75:
-        print("Grade B")
-    elif marks >= 60:
-        print("Grade C")
-    elif marks >= 40:
-        print("Grade D")
-    else:
-        print("Grade F")
+if not marks.isdigit():
+    print("Invalid input. Please enter a number between 0 and 100.")
+    sys.exit()
+
+marks = int(marks)
+
+if marks < 0 or marks > 100:
+    print("Invalid input. Marks should be between 0 and 100.")
+elif marks >= 90:
+    print("A")
+elif marks >= 75:
+    print("B")
+elif marks >= 60:
+    print("C")
+elif marks >= 40:
+    print("D")
+else:
+    print("F")
